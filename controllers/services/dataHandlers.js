d43a5534071c9179534410fca7b68f7518af0e40
Mika Ratilainen <mihail.ratilainen@yandex.ru>
2020-01-12 18:09:39 +0300

    minor fix

--- controllers/services/dataHandlers.js
+++ controllers/services/dataHandlers.js
@@ -1,9 +1,11 @@
 const { handleNotFound } = require('./handleNotFound');
 
 module.exports.handleNotFound = (data, res) => {
-  const isEmptyArray = data.length && data.length === 0;
-  const isNull = data === null;
-  const isNotFound = isEmptyArray || isNull;
+  const isArray = data.length !== undefined;
+  const isNotFound = isArray ? (data.length === 0) : data === null;
+  console.log('data', data);
+  console.log('isArray', isArray);
+  console.log('isNotFound', isNotFound);
 
   if (isNotFound) {
     handleNotFound(res);
